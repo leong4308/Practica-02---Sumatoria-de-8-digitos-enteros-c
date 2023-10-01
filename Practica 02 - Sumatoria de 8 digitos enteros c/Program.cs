48d0ea4576dad72e744dd74900d5a0cf3d59f2a4
leong <leong@DESKTOP-A61DB78>
2023-09-30 21:08:53 -0400

    Agregar archivos de proyecto.

---
+++ Practica 02 - Sumatoria de 8 digitos enteros c/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+
+{
+	const int NUM = 8;
+	static void Main()
+	{
+
+		int[] nums = new int[NUM];
+		int total = 0;
+		for (int i = 0; i < NUM; i++)
+
+		{
+			Console.Write("Por favor, introduzca el número: ");
+			nums[i] = int.Parse(Console.ReadLine());
+			total += nums[i];
+
+		}
+		Console.WriteLine("El total de números es: " + total);
+		Console.ReadKey();
+
+	}
+}
